143d17be3de826cc0ee0f37548fbf403262b061c
Rui Santiago <ruisantiagomr@gmail.com>
2022-09-07 20:34:46 +0100

    Add Missing DisableScroll prop

--- src/components/ReactImageCarouselViewer.tsx
+++ src/components/ReactImageCarouselViewer.tsx
@@ -5,10 +5,15 @@ import Modal from "./Modal";
 import "../styles/tailwind.css";
 
 export const ReactImageCarouselViewer = (props: ImageViewerProps) => {
-  const { open, onClose, images, startIndex, extraTopElement } = props;
+  const { open, onClose, images, startIndex, extraTopElement, disableScroll } =
+    props;
   return (
     <Modal open={open} onClose={onClose} extraTopElement={extraTopElement}>
-      <Carousel images={images} startIndex={startIndex} />
+      <Carousel
+        images={images}
+        startIndex={startIndex}
+        disableScroll={disableScroll}
+      />
     </Modal>
   );
 };
