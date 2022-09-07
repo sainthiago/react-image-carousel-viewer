f217918fd91de34f439dc7fe9dc783a1381714fc
Rui Santiago <ruisantiagomr@gmail.com>
2022-09-07 21:43:04 +0100

    Add Breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+        xxl: "1920px",
+      },
       padding: {
         "1/2": "50%",
       },
