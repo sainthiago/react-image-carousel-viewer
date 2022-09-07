56f4a0559e6fef57f2a52249af68703fb94ef489
Rui Santiago <ruisantiagomr@gmail.com>
2022-09-07 21:38:25 +0100

    Add new Loading Spinner

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -94,7 +94,7 @@ const Carousel = (props: CarouselProps) => {
               className="w-full max-h-full object-contain absolute top-1/2 transform -translate-y-1/2 right-0 left-0 bottom-0 md:py-16"
               src={images[currentIndex].src}
               onLoad={() => {
-                setIsLoading(false);
+                setTimeout(() => setIsLoading(false), 1000);
               }}
             />
             {images[currentIndex].description ? (
